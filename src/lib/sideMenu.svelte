1ff52359884187e772aaf16a339232e535692619
Kandreas9 <andkarido@gmail.com>
2023-02-08 23:05:43 +0100

    added background for menu and changed zindex

--- src/lib/sideMenu.svelte
+++ src/lib/sideMenu.svelte
@@ -18,6 +18,14 @@
 	};
 </script>
 
+<div
+	tabIndex={0}
+	on:keydown={(e) => handleKeyDown(e, handleCloseMenu)}
+	on:click={handleCloseMenu}
+	class="background"
+	class:backgroundOpen={menuOpen}
+/>
+
 <div class="sideMenu" class:menuOpen>
 	{#if !menuOpen}
 		<img
@@ -82,19 +90,33 @@
 </div>
 
 <style>
+	.background {
+		transition: opacity 0.7s ease-in-out, transform 0s 0.7s;
+		position: fixed;
+		top: 0;
+		right: 0;
+		z-index: 19;
+		width: 100vw;
+		height: 100vh;
+		background-color: rgba(0, 0, 0, 0.4);
+		transform: translateX(100%);
+		opacity: 0;
+	}
 	.sideMenu {
+		z-index: 20;
 		position: fixed;
 		top: 0;
 		right: 0;
 		height: 100vh;
-		width: 40vw;
-		transform: translateX(40vw);
+		width: 25vw;
+		transform: translateX(25vw);
 		background-color: rgb(47, 45, 67);
 		transition: transform 0.7s ease-in-out;
 		border-left: 2px solid rgb(95, 92, 126);
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		gap: 2rem;
 		padding: 4rem 0;
 	}
 
@@ -108,6 +130,12 @@
 
 	.menuOpen {
 		transform: translateX(0);
+	}
+
+	.backgroundOpen {
+		transform: translateX(0);
+		opacity: 1;
+		transition: opacity 0.7s ease-in-out;
 	}
 
 	.open,
